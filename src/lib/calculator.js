a171d188484848155879c65729299845e1b18d42
Aurino Geraldo <aurinoaj@gmail.com>
2021-06-18 12:06:07 -0300

    Create unit test for calculator

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,1 +1,8 @@
-module.exports.sum = (num1, num2) => num1 + num2
+module.exports.sum = (num1, num2) => {
+
+  if(isNaN(parseInt(num1, 10)) || isNaN(parseInt(num2, 10))){
+    throw new Error('Deu ruim veinho')
+  }
+  
+ return parseInt(num1, 10) + parseInt(num2, 10)
+}
--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -3,3 +3,18 @@ const { sum } = require('./calculator')
 it('should sum 2 and 2 and the result is 4', () => {
   expect(sum(2, 2)).toBe(4)
 });
+
+it('should sum "2" and 2 the result is 4', () => {
+  expect(sum('2', 2)).toBe(4)
+});
+
+it('should error sum "a" and 2', () => {
+  expect(() => sum('a', 2)).toThrowError()
+  expect(() => sum('', 2)).toThrowError()
+});
+
+it('should error when sum invalid parameters', () => {
+  expect(() => sum({})).toThrowError()
+  expect(() => sum([])).toThrowError()
+  expect(() => sum([2,2])).toThrowError()
+});
