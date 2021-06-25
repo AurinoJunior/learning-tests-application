7c7243e7b227aaa8bd876e92c8a30897d3893118
Aurino Geraldo <aurinoaj@gmail.com>
2021-06-25 09:35:00 -0300

    Add prettier

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,8 +1,7 @@
 module.exports.sum = (num1, num2) => {
-
-  if(isNaN(parseInt(num1, 10)) || isNaN(parseInt(num2, 10))){
-    throw new Error('Deu ruim veinho')
+  if (isNaN(parseInt(num1, 10)) || isNaN(parseInt(num2, 10))) {
+    throw new Error('Deu ruim veinho');
   }
-  
- return parseInt(num1, 10) + parseInt(num2, 10)
-}
+
+  return parseInt(num1, 10) + parseInt(num2, 10);
+};
